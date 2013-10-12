d5a96bb3649ca644113fc8878facddb4354d3acf
Edward Faulkner <ef@alum.mit.edu>
2013-10-12 17:53:54 -0400

    Properly order asynchronous `before` & `after`
    `before` and `after` handlers can be asynchronous, and they need to run
    in series.

--- pavlov.js
+++ pavlov.js
@@ -686,10 +686,10 @@
                 statements.push(function () {
                     QUnit.module(example.names(), {
                         setup: function () {
-                            each(befores, function () { this(); });
+                            QUnit.config.queue = befores.concat(QUnit.config.queue);
                         },
                         teardown: function () {
-                            each(afters, function () { this(); });
+                            QUnit.config.queue = QUnit.config.queue.concat(afters);
                         }
                     });
                 });
